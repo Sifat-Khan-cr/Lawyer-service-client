64509d81685cb450f025ebf3d27117e5d993c518
Sifat-Khan-cr <pskcrjr@gmail.com>
2022-11-08 23:25:13 +0600

    firebase added

--- src/Pages/Shared/Navbar/Navbar.js
+++ src/Pages/Shared/Navbar/Navbar.js
@@ -1,16 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     return (
         <div className="navbar bg-primary text-primary-content">
             <div className="flex-1">
-                <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+                <a className="btn btn-ghost normal-case text-xl">Lawyer</a>
             </div>
             <div className="flex-none">
                 <ul className="menu menu-horizontal p-0">
-                    <li><a>Item 1</a></li>
+                    <li><Link>Blog</Link></li>
+                    <li><Link>Contact</Link></li>
 
-                    <li><a>Item 3</a></li>
+
                 </ul>
             </div>
         </div>
