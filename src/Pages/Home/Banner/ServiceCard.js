d1b2105405d6bd813a65ffc80c764467a6e2eb0e
Sifat-Khan-cr <pskcrjr@gmail.com>
2022-11-10 01:04:39 +0600

    react photo view work done

--- src/Pages/Home/Banner/ServiceCard.js
+++ src/Pages/Home/Banner/ServiceCard.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { PhotoProvider, PhotoView } from 'react-photo-view';
+import 'react-photo-view/dist/react-photo-view.css';
 
 const ServiceCard = ({ service }) => {
     return (
         <div className="card w-96 bg-base-100 shadow-xl">
             <figure className="px-10 pt-10">
-                <img src={service.image} alt="Shoes" className="rounded-xl" />
+                <PhotoProvider>
+                    <PhotoView src={service.image}>
+                        {/* <img src="/1-thumbnail.jpg" alt="" /> */}
+                        <img src={service.image} alt="Shoes" className="rounded-xl" />
+                    </PhotoView>
+                </PhotoProvider>
             </figure>
             <div className="card-body items-center text-center">
                 <h2 className="card-title">{service.name}</h2>
@@ -14,8 +21,10 @@ const ServiceCard = ({ service }) => {
                     <Link to={`/services/${service._id}`}><button className="btn btn-primary">Details</button></Link>
                 </div>
             </div>
+
         </div>
     );
 };
 
 export default ServiceCard;
+
--- src/Pages/Home/Service.js
+++ src/Pages/Home/Service.js
@@ -3,12 +3,14 @@ import ServiceCard from './Banner/ServiceCard';
 
 const Service = () => {
     const [services, setServices] = useState([]);
+    const [lim, setLim] = useState(3);
+    // console.log(lim);
 
     useEffect(() => {
-        fetch('http://localhost:5000/services')
+        fetch(`http://localhost:5000/services?lim=${lim}`)
             .then(res => res.json())
             .then(data => setServices(data))
-    }, []);
+    }, [lim]);
     return (
         <div>
             <div className='flex justify-center'>
@@ -21,7 +23,7 @@ const Service = () => {
                 </div>
             </div>
             <div className=' my-5 mx-auto w-3/12'>
-                <button className="btn btn-success w-full">Show All</button>
+                <button onClick={() => setLim(0)} className="btn btn-success w-full">Show All</button>
             </div>
 
         </div>
