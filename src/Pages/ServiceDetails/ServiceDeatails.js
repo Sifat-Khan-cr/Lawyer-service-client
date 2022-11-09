db70ff61ad50118202104a68097b776417ba1266
Sifat-Khan-cr <pskcrjr@gmail.com>
2022-11-09 23:22:14 +0600

    service details page created

--- src/Pages/ServiceDetails/ServiceDeatails.js
+++ src/Pages/ServiceDetails/ServiceDeatails.js
@@ -6,9 +6,16 @@ const ServiceDeatails = () => {
     const { name, info, price, image } = details;
     console.log(price)
     return (
-        <div>
-            <h1>this is details</h1>
-
+        <div className="hero min-h-screen" style={{ backgroundImage: `url(${image})` }}>
+            <div className="hero-overlay bg-opacity-60"></div>
+            <div className="hero-content text-center text-neutral-content">
+                <div className="max-w-md">
+                    <h1 className="mb-5 text-6xl font-bold">{name}</h1>
+                    <p className="mb-5">{info}</p>
+                    <h1 className="mb-5 text-4xl font-bold text-white">${price}</h1>
+                    <button className="btn btn-primary">Contact</button>
+                </div>
+            </div>
         </div>
     );
 };
